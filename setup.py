b69de8317abdd7c61c8d1b7b1b072feb7ba28c58
Julian Winter <julian.winter@fh-muenster.de>
2018-12-20 12:27:56 +0100

    Added setup.py-file for pip.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name        = 'germalemma',
+      version     = '0.1',
+      description = 'lemmatizer for german language',
+      url         = 'https://github.com/WZBSocialScienceCenter/germalemma',
+      author      = 'Markus Konrad',
+      author_email= 'markus.konrad@wzb.eu',
+      license     = 'Apache License 2.0',
+      packages    = ['germalemma'],
+      #install_requires = [
+      #  'pyphen',
+      #  'pattern'
+      #],
+      zip_safe    = False)
